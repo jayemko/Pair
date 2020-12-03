7eaed04e29ef0ace8af5cd700710ffe85762206a
Jason Koceja <jason@koceja.com>
2020-12-03 09:32:03 -0800

    adds model object controller and view controller replaces and classes view controller in storyboard

---
+++ Pair/Controllers/ModelControllers/PairsController.swift
@@ -0,0 +1,28 @@
+//
+//  PairsController.swift
+//  Pair
+//
+//  Created by Jason Koceja on 12/3/20.
+//
+
+import Foundation
+
+
+class PairsController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Initializer
+    
+    // MARK: - Helpers
+    
+    // CRUD
+    //  create
+    //  update
+    //  delete
+    
+    // MARK: - Persistence
+    
+    // MARK: - Extensions
+    
+}
---
+++ Pair/Controllers/ViewControllers/PairsListTableViewController.swift
@@ -0,0 +1,48 @@
+//
+//  PairsListTableViewController.swift
+//  Pair
+//
+//  Created by Jason Koceja on 12/3/20.
+//
+
+import UIKit
+
+class PairsListTableViewController: UITableViewController {
+
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Actions
+    
+    // MARK: - Helpers
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+}
